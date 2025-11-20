2aeec40d8adc1f1fe58d7cb3f6f5d3b956966d95
Rafa-nc1724 <rniecam350@iesmarquesdecomares.com>
2025-11-20 11:37:42 +0100

    cositas

---
+++ apuntes/ejercicios_de_clase/POO/Empleado.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'Persona.php';
+class Empleado extends Persona{
+    private $salario;
+    
+    public function __construct($n = "Antonio", $a = "López", $e = 26,$sal=1500) {
+        parent::__construct($n, $a, $e);
+        $this->salario=$sal;
+    }
+    
+    public function __get(string $name): mixed {
+        return parent::__get($name);
+    }
+}
